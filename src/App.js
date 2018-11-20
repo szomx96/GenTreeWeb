19d64d131d499f6675500ef2fa5a29a50356fd7a
dominuka <jurczyk.dom@gmail.com>
2018-11-20 23:17:48 +0100

    structure

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import React, { Component } from 'react';
 import './App.css';
 //import { Stage, Layer, Rect, Text, Group } from 'react-konva';
 import faker from 'faker';
-import { Card, Icon, Image } from 'semantic-ui-react'
 import rd3 from 'react-d3-library'
+import CardTest from './Components/CardTest'
 
 
 
 const persons = [];
+
 for(let i = 0; i < 10; i++){
   persons.push({
     firstName: faker.name.firstName(),
@@ -22,28 +23,6 @@ for(let i = 0; i < 10; i++){
     desc: faker.lorem.sentence(),
     id: faker.random.number(),
   })
-}
-
-const CardTest = (props) => {
-  return(
-  <Card>
-    <Image src = {props.avatar} size="massive"/>
-    <Card.Content>
-      <Card.Header>{props.firstName} {props.lastName}</Card.Header>
-      <Card.Meta>
-        <span>{props.country}</span><br />
-        <span>{props.city}</span>
-        {/* <span className='date'>Birth date: {props.birthDate}</span> */}
-        {/* {props.deathDate && <span className='date'>Death date: {props.deathDate}</span>} */}
-      </Card.Meta>
-      <Card.Description>{props.desc}</Card.Description>
-    </Card.Content>
-    <Card.Content extra>
-      <a>
-        <Icon name = 'mail'></Icon>{props.email}
-      </a>
-    </Card.Content>
-  </Card>)
 }
 
 
---
+++ src/Components/CardTest.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { Card, Icon, Image } from 'semantic-ui-react'
+
+const CardTest = (props) => {
+    return(
+    <Card>
+      <Image src = {props.avatar} size="massive"/>
+      <Card.Content>
+        <Card.Header>{props.firstName} {props.lastName}</Card.Header>
+        <Card.Meta>
+          <span>{props.country}</span><br />
+          <span>{props.city}</span>
+          {/* <span className='date'>Birth date: {props.birthDate}</span> */}
+          {/* {props.deathDate && <span className='date'>Death date: {props.deathDate}</span>} */}
+        </Card.Meta>
+        <Card.Description>{props.desc}</Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <a>
+          <Icon name = 'mail'></Icon>{props.email}
+        </a>
+      </Card.Content>
+    </Card>)
+  }
+
+  export default CardTest;

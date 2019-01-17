7e8fb292f72c741170629b2bc39df143faecd66b
dominuka <jurczyk.dom@gmail.com>
2019-01-17 16:06:03 +0100

    users tree list

--- scripts/getTrees.js
+++ scripts/getTrees.js
@@ -6,27 +6,42 @@ document.addEventListener('DOMContentLoaded', function() {
   displayList();
 }, false);
 
+function displayList() {
 
-
-
-function displayList(){
-
-  //console.log(api.getUserTrees(data.id));
+  var res;
 
   api.getUserTrees(data.id)
-  .then(response =>{
-    console.log('Success:', response);
-  });
-  
+    .then(response => {
+      console.log('display:', response);
+      res = response;
+      var length = Object.keys(response[0]).length;
+      return length;
 
-  var table = document.getElementById("treeList");
-  var row = table.insertRow(0);
-  var cell1 = row.insertCell(0);
+    }).then(length => {
 
-  // var length = Object.keys(data.list[0]).length;
-  // console.log(length);
+      console.log(length);
 
-  cell1.innerHTML = "test";
-  
+      var table = document.getElementById("treeList");
+      var rows = [];
+      var cells = [];
 
+      for (var i = 0; i < length; i++) {
+        rows.push(table.insertRow(i));
+        cells.push(rows[i].insertCell(0));
+        cells[i].innerHTML = res[i].name;
+      }
+
+      for (var i = 0; i < cells.length; i++) {
+        cells[i].addEventListener("click", showSelectedTree(i, res));
+      }
+
+    });
+}
+
+function showSelectedTree(i, res) {
+  return function() {
+      console.log("you clicked region number " + i);
+      console.log(res[i].id);
+      //przekierowanie do maina i wyswietlenie drzewa z tym id
+  };
 }

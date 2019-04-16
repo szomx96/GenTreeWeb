bfcef8e8daa3edec4ca9fec302981c8876b11401
szomx96 <jurczyk.dom@gmail.com>
2019-04-16 11:45:35 +0200

    main script fixed

--- scripts/login.js
+++ scripts/login.js
@@ -8,28 +8,6 @@ document.getElementById("registerButton").addEventListener("click", function() {
     location.replace("./register.html");
 }, false);
 
-//                                  old login
-// function sendLogin() {
-//     console.log('sendLogin sie wywowal');
-// let status;
-
-//     api.postLogin(document.getElementById("name").value, document.getElementById("password").value) //no fajna abstrakcja, czy to ten no, boilerplate?
-//     .then(res => {status = res.status; return res.json()})
-//         .then(response => {
-//             console.log('Success:', JSON.stringify(response));
-//             if (status == 200) {                                      
-//                 localStorage.setItem('sessionData', JSON.stringify(response));
-//                 location.replace("./main.html");
-//             } else {
-//                 document.getElementById("infoLabel").innerHTML = response.message;
-//                 clearInput();
-//             }
-//         })
-//         .catch(error => console.error('Error:', error));
-    
-// }
-
-
 function sendLogin() {
     console.log('sendLogin sie wywowal');
 let status;
